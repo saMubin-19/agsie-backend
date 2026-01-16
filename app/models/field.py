753bf1afa6601c1544a2f6fb41af49258c427d61
saMubin-19 <almubin1903@gmail.com>
2026-01-16 22:12:51 +0600

    Update Field model schema

--- app/models/field.py
+++ app/models/field.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, Float, String
-from geoalchemy2 import Geometry
 from sqlalchemy.orm import declarative_base
+from geoalchemy2 import Geometry
 
 Base = declarative_base()
 
@@ -8,10 +8,12 @@ class Field(Base):
     __tablename__ = "fields"
 
     id = Column(Integer, primary_key=True, index=True)
+
     area_hectares = Column(Float, nullable=False)
     ndvi_status = Column(String, nullable=False)
 
     geometry = Column(
         Geometry(geometry_type="POLYGON", srid=4326),
-        nullable=False,
+        nullable=False
     )
+
